3e819cd3ccb5f9bcdf9ad3a15fd9e88e24185eb5
NewLord8951 <alexandrmaniraki@gmail.com>
2024-11-28 11:09:40 +0000

    1.16

--- task1.16.py
+++ task1.16.py
@@ -4,7 +4,7 @@ x = int(input("Введите число x: "))
 a = int(input("Введите число a: "))
 b = int(input("Введите число b: "))
 c = int(input("Введите число c: "))
-R = int(input("Введите число R: "))
+R = 8.31
 d = int(input("Введите число d: "))
 
 print(f"a) a / b / c = {round(a / b / c, 2)}")
