8c4a02fff34e599a858d7549a4cb60bdfcb4052f
NewLord8951 <alexandrmaniraki@gmail.com>
2025-01-14 09:47:51 +0000

    4.2

--- task4.2.py
+++ task4.2.py
@@ -1,8 +1,8 @@
 from math import sin, radians
-x = int(input("Введите число x: "))
+x = int(input("Введите число: "))
 if x > 0:
     primer_one = sin(radians(x ** 2))
     print(f"Ответ: {primer_one}")
 else:
-    primer_two = 1 - 2 * sin(radians(x)) ** 2
+    primer_two = 1 - 2 * sin(radians(x * x))
     print(f"Ответ: {primer_two}")
