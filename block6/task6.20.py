1f1b28e4db66abe46a26d246822ea7dd7fc064af
New_Lord_8951 <alexandrmaniraki@gmail.com>
2025-03-03 12:06:36 +0300

    6.20

--- block6/task6.20.py
+++ block6/task6.20.py
@@ -2,6 +2,6 @@ yn = int(input("Введите число n: "))
 x = int(input("Введите число x: "))
 e = int(input("Введите число e: "))
 if abs((yn ** 2) - ((yn - 1) ** 2) < e):
-    print(0.5 * ((yn - 1) + (x / ((yn - 1) - 1) )))
+    print(0.5 * ((yn - 1) + (x / ((yn - 1) - 1))))
 else:
     print("Ошибка")
